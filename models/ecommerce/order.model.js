e94ab6cf0a3d795224c2b15c5838b52cb490d40a
SOUMYA <soumya9521@gmail.com>
2024-03-07 19:20:09 +0530

    Schema creation

---
+++ models/ecommerce/order.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.productId,
+    ref: "Product",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.OrderId,
+      ref: "User",
+    },
+    orderItem: {
+      type: [orderItemSchema],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "CANCELED", "DELIVERED"],
+      default: "PENDING",
+    },
+  },
+  { timestamps: true }
+);
+export const Order = mongoose.model("Order", orderSchema);
